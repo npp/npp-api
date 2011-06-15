998d9ef73c5d15f6ae9c39fe664ad956e1024f6d
Becky Sweger <bsweger@nationalpriorities.org>
2011-06-15 14:50:39 -0400

    updates to USDA SNAP data

--- data/management/commands/import_snap_monthly_benefits_person.py
+++ data/management/commands/import_snap_monthly_benefits_person.py
@@ -1,7 +1,8 @@
 from django import db
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
-from data.models import SNAPMonthlyBenefitsPerson
+from django.core.exceptions import MultipleObjectsReturned
+from data.models import SnapMonthlyBenefitsPerson
 import csv
 
 # National Priorities Project Data Repository
@@ -9,7 +10,7 @@ import csv
 # Updated 7/27/2010, Joshua Ruihley, Sunlight Foundation
 
 # Imports USDA SNAP Benefits per Person by State
-# source info: http://www.fns.usda.gov/pd/18SNAPavg$PP.htm (accurate as of 7/27/2010)
+# source info: http://www.fns.usda.gov/pd/18SNAPavg$PP.htm (accurate as of 6/15/2011)
 # npp csv: http://assets.nationalpriorities.org/raw_data/hunger/snap_monthly_benefits_person.csv (updated 7/27/2010)
 # destination model:  SNAPMonthlyBenefitsPerson
 
@@ -17,8 +18,11 @@ import csv
 # 1) Download source files from url listed above
 # 2) Convert source file to .csv with same formatting as npp csv
 # 3) change SOURCE_FILE variable to the the path of the source file you just created
-# 4) change 'amount' column in data_SNAPMonthlyBenefitsPerson table to type 'bigint'
+# 4) change 'amount' column in data_SnapMonthlyBenefitsPerson table to type 'bigint'
 # 5) Run as Django management command from your project path "python manage.py import_snap_monthly_benefits_person"
+
+# Safe to re-run: YES (if year/state record is already
+# loaded, program will update the value rather than insert a new record)
 
 SOURCE_FILE = '%s/hunger/snap_monthly_benefits_person.csv' % (settings.LOCAL_DATA_ROOT)
 
@@ -40,11 +44,29 @@ class Command(NoArgsCommand):
                 year_row = row;            
             else:
                 state = row[0]
-                for j,col in enumerate(row):
-                    if j > 0:
-                        record = SNAPMonthlyBenefitsPerson()
-                        record.year = int(year_row[j])
-                        record.state = state
-                        record.value = clean_float(col)
-                        record.save()
-                        db.reset_queries()
+                if len(state):
+                    for j,col in enumerate(row):
+                        if j > 0:
+                        
+                            try:
+                                #if year & state already exist, update the value (previous years' data is often revised)
+                                record = SnapMonthlyBenefitsPerson.objects.get(state=state, year=int(year_row[j]))
+                                current_value = clean_float(col)
+                                if record.value != current_value:
+                                    print str(year_row[j]) + ' ' + state + ' updating from ' + str(record.value) + ' to ' + str(current_value)
+                                    record.value = current_value
+                                    record.save()
+                                
+                            except MultipleObjectsReturned:
+                                print 'error: multiple records exist for ' + str(year_row[j]) + ' ' + state
+                                continue
+                                
+                            except:
+                                #this year & state isn't in the db yet; insert
+                                record = SnapMonthlyBenefitsPerson()
+                                record.year = int(year_row[j])
+                                record.state = state
+                                record.value = clean_float(col)
+                                record.save()
+                                
+        db.reset_queries()
--- data/management/commands/import_snap_participation_households.py
+++ data/management/commands/import_snap_participation_households.py
@@ -1,7 +1,8 @@
 from django import db
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
-from data.models import SNAPParticipationHouseholds
+from django.core.exceptions import MultipleObjectsReturned
+from data.models import SnapParticipationHouseholds
 import csv
 
 # National Priorities Project Data Repository
@@ -9,9 +10,9 @@ import csv
 # Updated 7/27/2010, Joshua Ruihley, Sunlight Foundation
 
 # Imports USDA Number of Households Participating in Foodstamp Program per State
-# source info: http://www.fns.usda.gov/pd/16SNAPpartHH.htm (accurate as of 7/27/2010)
+# source info: http://www.fns.usda.gov/pd/16SNAPpartHH.htm (accurate as of 6/15/2011)
 # npp csv: http://assets.nationalpriorities.org/raw_data/hunger/snap_participation_households.csv (updated 7/27/2010)
-# destination model:  SNAPParticipationHouseholds
+# destination model:  SnapParticipationHouseholds
 
 # HOWTO:
 # 1) Download source files from url listed above
@@ -19,6 +20,9 @@ import csv
 # 3) change SOURCE_FILE variable to the the path of the source file you just created
 # 4) change 'amount' column in data_SNAPParticipationHouseholds table to type 'bigint'
 # 5) Run as Django management command from your project path "python manage.py import_snap_participation_households"
+
+# Safe to re-run: YES (if year/state record is already
+# loaded, program will update the value rather than insert a new record)
 
 SOURCE_FILE = '%s/hunger/snap_participation_households.csv' % (settings.LOCAL_DATA_ROOT)
 
@@ -40,11 +44,29 @@ class Command(NoArgsCommand):
                 year_row = row;            
             else:
                 state = row[0]
-                for j,col in enumerate(row):
-                    if j > 0:
-                        record = SNAPParticipationHouseholds()
-                        record.year = int(year_row[j])
-                        record.state = state
-                        record.value = clean_int(col)
-                        record.save()
-                        db.reset_queries()
+                if len(state):
+                    for j,col in enumerate(row):
+                        if j > 0:
+                        
+                            try:
+                                #if year & state already exist, update the value (previous years' data is often revised)
+                                record = SnapParticipationHouseholds.objects.get(state=state, year=int(year_row[j]))
+                                current_value = clean_int(col)
+                                if record.value != current_value:
+                                    print str(year_row[j]) + ' ' + state + ' updating from ' + str(record.value) + ' to ' + str(current_value)
+                                    record.value = current_value
+                                    record.save()
+                                
+                            except MultipleObjectsReturned:
+                                print 'error: multiple records exist for ' + str(year_row[j]) + ' ' + state
+                                continue
+                                
+                            except:
+                                #this year & state isn't in the db yet; insert
+                                record = SnapParticipationHouseholds()
+                                record.year = int(year_row[j])
+                                record.state = state
+                                record.value = clean_int(col)
+                                record.save()
+                           
+        db.reset_queries()
