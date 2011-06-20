e385426d3b8b4bed3bdd52ed81db880bd2bdb3e5
Becky Sweger <bsweger@nationalpriorities.org>
2011-06-20 12:45:53 -0400

    updates to medicare enrollment & medicaid participation imports

--- data/management/commands/import_medicaid_participation.py
+++ data/management/commands/import_medicaid_participation.py
@@ -1,6 +1,7 @@
 from django import db
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
+from django.core.exceptions import MultipleObjectsReturned
 from data.models import MedicaidParticipation
 import csv
 
@@ -9,15 +10,17 @@ import csv
 # Updated 6/18/2010, Joshua Ruihley, Sunlight Foundation
 
 # Imports HHS State Medicaid Participation
-# source info: http://www.cms.hhs.gov/MedicareMedicaidStatSupp/LT/list.asp?filterType=none&filterByDID=0&sortByDID=1&sortOrder=ascending&intNumPerPage=10&listpage=3 (accurate as of 6/18/2010)
+# source info: http://www.census.gov/compendia/statab/cats/health_nutrition/medicare_medicaid.html (table 148) (accurate as of 6/20/2011)
 # npp csv: http://assets.nationalpriorities.org/raw_data/health/medicaid_participation.csv (updated 6/18/2010)
 # destination model:  MedicaidParticipation
 
 # HOWTO:
 # 1) Download source files from url listed above
-# 2) Convert source file to .csv with same formatting as npp csv
+# 2) Convert source file to .csv with same formatting as npp csv.  Make sure numbers are still expressed in thousands (import will handle--just ensure that's still the case)
 # 3) change SOURCE_FILE variable to the the path of the source file you just created
 # 5) Run as Django management command from your project path "python manage.py import_medicaid_participation
+
+# Safe to re-run: YES. Previous years' numbers can be revised.
 
 SOURCE_FILE = '%s/health/medicaid_participation.csv' % (settings.LOCAL_DATA_ROOT)
 
@@ -28,7 +31,8 @@ class Command(NoArgsCommand):
         
         def clean_int(value):
             if value <> '':
-                value = int(value.replace(',', '').replace(' ', ''))
+                value = float(value.replace(',','').replace(' ', ''))
+                value = int(value * 1000)
             else:
                 value = None
             return value
@@ -39,9 +43,19 @@ class Command(NoArgsCommand):
             else:
                 for j,col in enumerate(row):
                     if j == 0:
-                        state = col
+                        state = col.strip()
                     elif j > 0:
-                        year = year_row[j]
-                        value = col
-                        record = MedicaidParticipation(state=state, year=year, value=clean_int(value))
-                        record.save()
+                        if len(state):
+                            year = year_row[j]
+                            try:
+                                record = MedicaidParticipation.objects.get(state=state, year=year)
+                            except MultipleObjectsReturned:
+                                print 'error: multiple records exist for ' + str(year_row[j]) + ' ' + state
+                                continue
+                            except:
+                                record = MedicaidParticipation()
+                                record.state = state
+                                record.year = year
+                            value = col
+                            record.value = clean_int(value)
+                            record.save()
