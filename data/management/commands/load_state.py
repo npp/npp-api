681701d5ceb6a5d502793f8d0d93268d3d59179a
Becky Sweger <bsweger@nationalpriorities.org>
2011-08-02 15:58:14 -0400

    add some missing territories to state load (they're not in the online state ANSI file) and make state load process re-runnable

--- data/management/commands/load_state.py
+++ data/management/commands/load_state.py
@@ -15,13 +15,52 @@ from data.models import AnsiState, State
 # 1) Ensure that AnsiState model is loaded and up to date
 # 2) Run as Django management command from your project path "python manage.py load_state
 
+# Safe to re-run?  Yes.
+
 class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
+    
+        update_count = 0
+        insert_count = 0
+        
         for s in AnsiState.objects.all():
-            state_ref_row = State(state_ansi=s.ansi_state, state_abbr = s.state, state_name = s.state_name, state_gnisid = s.gnisid)
-            state_ref_row.save()
-            db.reset_queries() 
-        state_ref_row = State(state_ansi='99', state_abbr='UD', state_name='U.S. undistributed')
-        state_ref_row.save()
+            state_change = False
+            try:
+                state_ref_row = State.objects.get(state_ansi=s.ansi_state)
+                if state_ref_row.state_abbr <> s.state:
+                    state_change = True
+                    state_ref_row.state_abbr = s.state
+                if state_ref_row.state_name <> s.state_name:
+                    state_change = True
+                    state_ref_row.state_name = s.state_name
+                if state_ref_row.state_gnisid <> s.gnisid:
+                    state_change = True
+                    state_ref_row.state_gnisid = s.gnisid
+                if state_change:
+                    state_ref_row.save()
+                    print str(s.ansi_state) + '(' + str(s.state) + ') updated'
+                    update_count = update_count + 1
+            except:
+                state_ref_row = State(state_ansi=s.ansi_state, state_abbr = s.state, state_name = s.state_name, state_gnisid = s.gnisid)
+                state_ref_row.save()
+                insert_count = insert_count + 1
+                print str(s.ansi_state) + '(' + str(s.state) + ') inserted'
+                
+        # a few ancillary state records (e.g., Undistributed funds, terriories not in the state ansi file)
+        more_states = {}
+        more_states['99'] = ['UD','U.S. undistributed']
+        more_states['64'] = ['FM','Federated States of Micronesia']
+        more_states['68'] = ['MH', 'Marshall Islands']
+        more_states['70'] = ['PW', 'Palau']
+        for s in more_states:
+            try:
+                state_ref_row = State(state_ansi=s, state_abbr = more_states[s][0], state_name = more_states[s][1])
+                state_ref_row.save()
+                insert_count = insert_count + 1
+                print str(s) + '(' + str(more_states[s][0]) + ') inserted'
+            except:
+                continue
+        
         db.reset_queries()
+        
