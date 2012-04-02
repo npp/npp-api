3b68ba0d5991efb6cd7800d136c7e367ca93a39d
Becky Sweger <bsweger@nationalpriorities.org>
2012-04-02 17:05:34 -0400

    update script to import new aids cases--allow re-run, augment comments re: source file location

--- data/management/commands/import_new_aids_cases.py
+++ data/management/commands/import_new_aids_cases.py
@@ -1,15 +1,19 @@
 from django import db
 from django.conf import settings
 from django.core.management.base import NoArgsCommand
+from django.core.exceptions import MultipleObjectsReturned
 from data.models import NewAidsCases
 import csv
+import sys
+from npp_api.data.utils import clean_num
 
 # National Priorities Project Data Repository
 # import_new_aids_cases.py
-# Updated 6/17/2010, Joshua Ruihley, Sunlight Foundation
 
 # Imports HHS State New AIDS Cases Data
 # source info: latest years are stored in PDF format.  for the 6/2011 updated, we pulled last several years manually into the old copy of aids.csv :(
+#   http://www.cdc.gov/hiv/topics/surveillance/resources/reports/#surveillance-->HIV Surveillance Report (Diagnosis of HIV Infection
+#   and AIDS in the United States and Dependent Areas, 2010) --> Table 20 AIDS diagnosis, by area of residence
 # npp csv: http://assets.nationalpriorities.org/raw_data/health/aids.csv (updated 6/17/2010)
 # destination model:  NewAIDSCases
 
@@ -27,23 +31,30 @@ class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
         data_reader = csv.reader(open(SOURCE_FILE))
-        
-        def clean_int(value):
-            if value <> '':
-                value = int(value.replace(',', '').replace(' ', ''))
-            else:
-                value = None
-            return value
-        
+        insert_count = 0
+        update_count = 0
         for i, row in enumerate(data_reader):
             if i == 0:
                 year_row = row;            
             else:
                 for j,col in enumerate(row):
                     if j == 0:
-                        state = col
+                        state = col.strip()
                     elif j > 0:
                         year = year_row[j]
-                        value = col
-                        record = NewAidsCases(state=state, year=year, value=clean_int(value))
-                        record.save()
+                        try:
+                            record = NewAidsCases.objects.get(state=state,year=year)
+                            if record.value <> clean_num(col):
+                                record.value = clean_num(col)
+                                record.save()
+                                db.reset_queries()
+                                update_count = update_count + 1
+                        except MultipleObjectsReturned:
+                            print 'error: multiple records exist for %s %s' % (year, state)
+                            continue
+                        except:
+                            record = NewAidsCases(year = year, state=state, value = clean_num(col))
+                            record.save()
+                            db.reset_queries()
+                            insert_count = insert_count + 1
+        print '%s import complete. %s records updated, %s inserted' % (SOURCE_FILE, update_count, insert_count)
