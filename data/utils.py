e1a4d4eb99842daea9f056df6108516f60603fb1
Becky Sweger <bsweger@nationalpriorities.org>
2012-04-25 15:42:59 -0400

    if value is not-numeric, return None

--- data/utils.py
+++ data/utils.py
@@ -6,12 +6,16 @@ def get_percent(numer,denom):
     return Decimal(str(float(numer)/denom*100)).quantize(Decimal('1.01'))
     
 def clean_num(value):
-    if value.strip()=='':
+    try:
+        if value.strip()=='':
+            value=None
+        elif value.find(".") <> -1:
+            value = value.replace(",","")
+            value = float(value)
+        else:
+            value = value.replace(",","")
+            value = int(value)
+    except:
+        #value isn't numeric, so return None
         value=None
-    elif value.find(".") <> -1:
-        value = value.replace(",","")
-        value = float(value)
-    else:
-        value = value.replace(",","")
-        value = int(value)
     return value
