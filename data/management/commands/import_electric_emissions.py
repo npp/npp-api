73eb59d590e0b98e78c0742eca50c3c447368d03
bsweger <bsweger@gmail.com>
2013-04-10 15:16:17 -0400

    changes for update to electric emissions data

--- data/management/commands/import_electric_emissions.py
+++ data/management/commands/import_electric_emissions.py
@@ -10,25 +10,22 @@ from npp_api.data.utils import clean_num
 
 # Imports Electric Power Estimate Emissions by state
 # source info: http://www.eia.gov/electricity/data.cfm#elecenv 
-#   (accurate as of 4/20/2012)
+#   (accurate as of 4/10/2013)
 # source file: http://www.eia.gov/cneaf/electricity/epa/emission_state.xls
-#   (accurate as of 4/20/2012)
+#   (accurate as of 4/10/2013)
 # destination model:  ElectricEmissionsStateRaw
 
 # HOWTO:
 # 1) Download source files from url listed above
 # 2) Convert source file to .csv with same formatting as npp csv 
-#       rename US-TOTAL to US)
 # 3) change SOURCE_FILE variable to the the path of the 
 #       source file you just created
-# 4) convert co2, so2 and nox columns in database to type bigint
-# 5) Run as Django management command from your project path 
+# 4) Run as Django management command from your project path 
 #       "python manage.py import_electric_emissions"
 
-# Safe to re-run: YES (if year/state record is already
-# loaded, program will update the value rather than insert a new record)
+# Safe to re-run: YES
 # NOTE: since the file always goes back to 1990, you can also delete the 
-#   currently data and reload from scratch.
+#   current data and reload from scratch.
 
 
 SOURCE_FILE = '%s/energy/emission_state.csv' % (settings.LOCAL_DATA_ROOT)
@@ -37,10 +34,8 @@ class Command(NoArgsCommand):
     
     def handle_noargs(self, **options):
         data_reader = csv.reader(open(SOURCE_FILE))
-        error_count = 0
         update_count = 0
         insert_count = 0
-        unchanged_count = 0
         for i, row in enumerate(data_reader):
             if i > 0:
                 year = row[0]
@@ -52,41 +47,36 @@ class Command(NoArgsCommand):
                         state = row[1]
                     producer_type = row[2]
                     energy_source = row[3]
-                    co2 = clean_num(row[4])
-                    so2 = clean_num(row[5])
-                    nox = clean_num(row[6])
-                    
+                    if row[4].lower().find('-') >= 0:
+                        co2 = 0
+                    else:
+                        co2 = clean_num(row[4])
+                    if row[5].lower().find('-') >= 0:
+                        so2 = 0
+                    else:
+                        so2 = clean_num(row[5])
+                    if row[6].lower().find('- ') >= 0:
+                        nox = 0
+                    else:
+                        nox = clean_num(row[6])
+
                     try:
                         record = ElectricEmissionsStateRaw.objects.get(
                             year=year, state=state, 
                             producer_type = producer_type,
                             energy_source = energy_source)
-                        update = False
-                        if record.co2 != co2:
-                            update = True
-                            record.co2 = co2
-                        if record.so2 != so2:
-                            update = True
-                            record.so2 = so2
-                        if record.nox != nox:
-                            update = True
-                            record.nox = nox
-                        if update:
-                            record.save()
-                            db.reset_queries()
-                            update_count = update_count + 1
-                        else:
-                            unchanged_count = unchanged_count + 1
-  
+                        record.c02 = co2
+                        record.s02 = s02
+                        record.nox = nox
+                        update_count = update_count + 1  
                     except:
                         record = ElectricEmissionsStateRaw(year=year, state=state, 
                             producer_type=producer_type,
                             energy_source=energy_source, 
                             co2=co2, so2=so2, nox=nox)
-                        record.save()
-                        db.reset_queries()
                         insert_count = insert_count + 1
-                        
-        print 'Emissions import complete. %s records inserted, %s updated, and %s unchanged.' % (insert_count, update_count, unchanged_count)
-        print '%s errors' % error_count
+                    record.save()
+                    db.reset_queries()
+
+        print 'Emissions import complete. %s records inserted, %s updated.' % (insert_count, update_count)
         
